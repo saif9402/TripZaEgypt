db135c88f01d026c75392db867374e7fa95e9635
saif9402 <saif.ufo@gmail.com>
2025-08-20 16:09:24 +0300

    completing the data whin using sign in with google

---
+++ src/JS/force-profile.js
@@ -0,0 +1,16 @@
+(function () {
+  const p = location.pathname.toLowerCase();
+  const isAuthPage =
+    /sign-in\.html|sign-up\.html|forgetpassword|oauth-callback/i.test(p);
+  if (isAuthPage) return;
+
+  // If the flag is set, force the user to profile page
+  if (localStorage.getItem("mustCompleteProfile") === "1") {
+    if (!/profile\.html$/i.test(p)) {
+      // remember where they were going
+      sessionStorage.setItem("afterProfile", location.href);
+      // adjust the path if your pages folder differs
+      location.replace("/pages/profile.html");
+    }
+  }
+})();
